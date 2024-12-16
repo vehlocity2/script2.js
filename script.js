c8cdcb52555f85999f00a87c2fb6c2d834bd7ea4
Oshioke <oshiokeboss@gmail.com>
2024-12-16 14:48:20 +0100

    My First Git Project

---
+++ script.js
@@ -0,0 +1,82 @@
+// console.log("Anthony");
+// const string = "seabutton";
+
+
+// function countVowel(string){
+//     const vowel = "aeiouAEIOU";
+//     let count = 0;
+//     for (let i = 0; i < string.length; i++){
+//         if (vowel.includes(string[i])){
+//             count++;
+//         }
+//     }
+//     return count
+// }
+// result = countVowel(string);
+// console.log(result);
+
+// 
+
+// let string = "seabutton";
+// let index = string.charAt("6");
+// console.log(index);
+
+// let array = ['sea','cats','dog','pig','goat'];
+// let index = array.charAt(3);
+// console.log(index);
+
+// let text = "Hello world, welcome to the universe.";
+// text.indexOf("e", 5);
+
+// let string = 'seabutton';
+// let reversedString = string.split('').reverse().join('');
+// let upper = string.toUpperCase();
+
+// console.log(upper);
+
+// let classMates = ['Emma','james','Gift','Peace'];
+// let reversedMates = classMates.reverse();
+// console.log(reversedMates);
+
+// let text = "HELLO WORLD";
+// let char = text.charAt(0);
+// console.log(char)
+
+// let name1 = "emma";
+// let text2 = "and";
+// let name2 = "josh";
+// let text3 = name1.concat(" ", text2, " ", name2,);
+
+// console.log(text3);
+
+// let text = ["Please" ,"locate" ,"where" ,'locate' ,"occurs!"];
+// let index = text.lastIndexOf("locate");
+// console.log(index);
+
+// let text = `sammy said "i'd love to see you"`;
+// let boy = text.split(" ");
+// let index = boy.lastIndexOf("see")
+// console.log(index);
+
+
+// let fruit = "bole";
+// let x = 5;
+// let sum = fruit + x;
+// console.log(sum);
+
+
+// let x = 123
+// x.toString();
+// console.log(x);
+
+
+let human = ['boy','girl','cat']
+let firstItem = human[0];
+human[0] = 'snake';
+console.log(human);
+
+
+
+
+
+// let text = `sammy said "i'd love to see you"`;
